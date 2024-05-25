5f128b025484cf5e90b25492ff7facd54baaeb79
Albert <albert.karagulov@mail.ru>
2024-05-25 23:06:08 +0300

    Created Add transaction functional to server

--- src/components/AddTransaction/index.jsx
+++ src/components/AddTransaction/index.jsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect, useState } from 'react';
 
 import styles from './AddTransaction.module.scss';
+import supabase from '../../config/supabaseConfig';
 import { CategoryChoosing } from '../CategoryChoosing';
 
 export const AddTransaction = ({
@@ -57,24 +58,141 @@ export const AddTransaction = ({
 	const [categoriesOpen, setCategoriesOpen] = useState(false);
 
 	const [formData, setFormData] = useState({
-		transactionType: typeTransactionClick,
+		transactionType: '',
 		transactionCategory: '',
 		amount: '',
 		date: '',
 		description: '',
 	});
 
+	const [categories, setCategories] = useState([]);
+
 	const handleFormDataChange = e => {
 		const { name, value } = e.target;
+		let formattedValue = value;
+		if (name === 'amount') {
+			formattedValue = value.replace(/[^\d.]/g, '');
+			const parts = formattedValue.split('.');
+			parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+			if (parts[1] && parts[1].length > 2) {
+				parts[1] = parts[1].slice(0, 2);
+			}
+			formattedValue = parts.join('.');
+		}
 		setFormData({
 			...formData,
-			[name]: value,
+			[name]: formattedValue,
 		});
+	};
+
+	const handleCategorySelect = categoryId => {
+		setFormData({
+			...formData,
+			transactionCategory: categoryId,
+		});
+	};
+
+	useEffect(() => {
+		// Функция для запроса к transaction_types
+		const fetchTransactionTypes = async () => {
+			const { data, error } = await supabase
+				.from('transaction_types')
+				.select('id, name');
+
+			if (error) {
+				console.error('Ошибка при получении данных из Supabase:', error);
+				return;
+			}
+
+			const incomeType = data.find(type => type.name === 'Доходы');
+			const expensesType = data.find(type => type.name === 'Расходы');
+
+			if (typeTransactionClick === 'income' && incomeType) {
+				setFormData(prevFormData => ({
+					...prevFormData,
+					transactionType: incomeType.id,
+				}));
+			} else if (typeTransactionClick === 'expenses' && expensesType) {
+				setFormData(prevFormData => ({
+					...prevFormData,
+					transactionType: expensesType.id,
+				}));
+			}
+		};
+
+		fetchTransactionTypes();
+	}, [typeTransactionClick]);
+
+	useEffect(() => {
+		// Функция для запроса к transaction_categories
+		const fetchTransactionCategories = async () => {
+			if (formData.transactionType) {
+				const { data, error } = await supabase
+					.from('transaction_categories')
+					.select('*')
+					.eq('transaction_type_id', formData.transactionType);
+
+				if (error) {
+					console.error('Ошибка при получении категорий из Supabase:', error);
+					return;
+				}
+
+				setCategories(data);
+			}
+		};
+
+		fetchTransactionCategories();
+	}, [formData.transactionType]);
+
+	const [validationErrors, setValidationErrors] = useState();
+
+	const addTransactionToDatabase = async () => {
+		try {
+			const { data, error } = await supabase.from('transactions').insert([
+				{
+					user_id: JSON.parse(localStorage.getItem('userState'))?.auth?.user
+						?.id,
+					date: formData.date,
+					amount: parseFloat(formData.amount.replace(/\s/g, '')).toFixed(2),
+					description: formData.description,
+					transaction_type_id: formData.transactionType,
+					transaction_category_id: formData.transactionCategory,
+				},
+			]);
+
+			if (error) {
+				console.error('Ошибка при добавлении транзакции:', error.message);
+				return;
+			}
+
+			console.log('Транзакция успешно добавлена:', data);
+		} catch (error) {
+			console.error('Ошибка при добавлении транзакции:', error.message);
+		}
+	};
+
+	const handleFormSubmit = async e => {
+		e.preventDefault();
+
+		if (!formData.transactionCategory || !formData.amount || !formData.date) {
+			setValidationErrors(
+				'Поля категории, суммы и даты должны быть заполнены!'
+			);
+			return;
+		}
+
+		setValidationErrors();
+		await addTransactionToDatabase();
+		setAddTransactionOpen(false);
 	};
 
 	return (
 		<div className={styles.add} ref={addRef}>
-			<form className={styles.addBlock} ref={addBlockRef}>
+			<form
+				className={styles.addBlock}
+				ref={addBlockRef}
+				onSubmit={handleFormSubmit}
+			>
 				<h3 className={styles.addBlock__title}>Добавить операцию</h3>
 				<div className={styles.addBlock__types}>
 					<button
@@ -82,6 +200,11 @@ export const AddTransaction = ({
 						onClick={() => {
 							setTypeTransactionClick('income');
 							setCategoriesOpen(false);
+							setCategories([]);
+							setFormData(prevFormData => ({
+								...prevFormData,
+								transactionCategory: '',
+							}));
 						}}
 						type='button'
 					>
@@ -92,6 +215,11 @@ export const AddTransaction = ({
 						onClick={() => {
 							setTypeTransactionClick('expenses');
 							setCategoriesOpen(false);
+							setCategories([]);
+							setFormData(prevFormData => ({
+								...prevFormData,
+								transactionCategory: '',
+							}));
 						}}
 						type='button'
 					>
@@ -103,8 +231,14 @@ export const AddTransaction = ({
 						className={styles.addBlock__inputsSelect}
 						onClick={() => setCategoriesOpen(!categoriesOpen)}
 					>
-						<p className={styles.addBlock__inputsSelect__text}>
-							{formData.transactionCategory || 'Категория'}
+						<p
+							className={`${styles.addBlock__inputsSelect__text} ${
+								formData.transactionCategory &&
+								styles.addBlock__inputsSelect__text_selected
+							}`}
+						>
+							{categories.find(cat => cat.id === formData.transactionCategory)
+								?.name || 'Категория'}
 						</p>
 						<div
 							className={`${styles.addBlock__inputsSelect__icon} ${
@@ -129,16 +263,8 @@ export const AddTransaction = ({
 						</div>
 						{categoriesOpen && (
 							<CategoryChoosing
-								categoriesData={[
-									'Зарплата',
-									'Стипендия',
-									'Пенсия',
-									'Подработка',
-									'Бизнес',
-									'Другое',
-									'Другое',
-									'Другое',
-								]}
+								categoriesData={categories}
+								onCategorySelect={handleCategorySelect}
 							/>
 						)}
 					</div>
@@ -156,8 +282,9 @@ export const AddTransaction = ({
 						value={formData.date}
 						onChange={handleFormDataChange}
 						placeholder='Дата'
-						type='text'
+						type='datetime-local'
 					/>
+
 					<textarea
 						className={styles.addBlock__inputsTextarea}
 						name='description'
@@ -166,6 +293,9 @@ export const AddTransaction = ({
 						placeholder='Заметка'
 					></textarea>
 				</div>
+				{validationErrors && (
+					<p className={styles.addBlock__error}>{validationErrors}</p>
+				)}
 				<button className={styles.addBlock__submit} type='submit'>
 					Добавить
 				</button>
--- src/components/CategoryChoosing/index.jsx
+++ src/components/CategoryChoosing/index.jsx
@@ -1,13 +1,23 @@
 import styles from './CategoryChoosing.module.scss';
 
-export const CategoryChoosing = ({ categoriesData }) => {
+export const CategoryChoosing = ({ categoriesData, onCategorySelect }) => {
 	return (
 		<ul className={styles.categories}>
-			{categoriesData.map((data, index) => (
-				<li className={styles.categoriesItem} key={index}>
-					{data}
+			{categoriesData.length > 0 ? (
+				categoriesData.map((category, index) => (
+					<li
+						className={styles.categoriesItem}
+						key={index}
+						onClick={() => onCategorySelect(category.id)}
+					>
+						{category.name}
+					</li>
+				))
+			) : (
+				<li className={styles.categoriesItem}>
+					Категории не найдены. Пожалуйста, создайте категории.
 				</li>
-			))}
+			)}
 		</ul>
 	);
 };
